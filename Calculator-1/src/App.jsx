866061c234598ae09bda5a0ff2635b211a892933
apoorvam07 <apoorvakshtriya18@gmail.com>
2023-12-23 01:10:56 +0530

    commit

---
+++ Calculator-1/src/App.jsx
@@ -0,0 +1,30 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.calculator}>
+      <input className={styles.display} type="text" />
+      <div className={styles.buttonscontainer}>
+        <button className={styles.button}>C</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>+</button>
+        <button className={styles.button}>3</button>
+        <button className={styles.button}>4</button>
+        <button className={styles.button}>-</button>
+        <button className={styles.button}>5</button>
+        <button className={styles.button}>6</button>
+        <button className={styles.button}>*</button>
+        <button className={styles.button}>7</button>
+        <button className={styles.button}>8</button>
+        <button className={styles.button}>/</button>
+        <button className={styles.button}>=</button>
+        <button className={styles.button}>9</button>
+        <button className={styles.button}>0</button>
+        <button className={styles.button}>.</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
